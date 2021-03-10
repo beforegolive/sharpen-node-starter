f460a2a082e5acfe5c892bfba73a4aa7592430b6
twomeetings <2326220@163.com>
2021-03-11 05:38:29 +0800

    通过ts-jest,成功配置jest

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import firstModule from './import-module'
+// import firstModule from './import-module'
 console.log('Hello World')
-const a = 123
-console.log(a)
-console.log(firstModule())
+// const a = 123
+// console.log(a)
+// console.log(firstModule())
